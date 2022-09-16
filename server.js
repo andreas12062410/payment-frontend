3f1381e754721b6f3df49f3f6982b9488ec6ddb6
Kartikey Kushwah <kartikey@koders.in>
2022-09-16 13:35:18 +0530

    fetched project issues

--- server.js
+++ server.js
@@ -1,58 +1,34 @@
-const Redmine = require('node-redmine');
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-DEBUG = True // ! Change to false in production
-
-const redmineObj = (host, api) => {
-    /**
-     * @param {string} host
-     * @param {string} api
-     * @returns {string}  the object of Redmine class is returned
-     */
-    const config = {
-      apiKey: api,
-    };
-    return new Redmine(host, config);
-};
+DEBUG = true // ! Change to false in production
 
 app.use(express.json())
+
+apiKey = ""
+projectIdentifier = "89"
+
+const getProjectIssues = async (apiKey, projectIdentifier) => {
+    try {
+      const response = await axios.get(`https://kore.koders.in/projects/${projectIdentifier}/issues.json`, {headers: {'X-Redmine-API-Key': apiKey}})
+      return response.data.issues
+    } catch (error) {
+      console.error(error)
+    }
+}
+
+(async() =>console.log(await getProjectIssues(apiKey, projectIdentifier)))()
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
 app.post('/milestones/', (req, res) => {
-    try{
-        const { apiKey, projectIdentifier } = req.body;
- 
-        // protocol required in Hostname, supports both HTTP and HTTPS
-        const redmine = redmineObj("https://kore.koders.in", apiKey);
-         
-        redmine.projects({}, fetchData = (data, err) => {
-
-
-        })
-
-        // redmine.issues({limit: 2}, function(err, data) {
-        //   if (err) throw err;
-         
-        //   for (let i in data.issues) {
-        //     dump_issue(data.issues[i]);
-        //   }
-         
-        //   console.log('total_count: ' + data.total_count);
-        // });
-    }
-    catch (error) {
-        if (DEBUG === True)
-            console.log(`Something went wrong. Please try again. Reason ${error}`)
-        else
-            console.log("Something went wrong. Please try again.")
-    }
-
+    const { apiKey, projectIdentifier } = req.body;
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
