009239306d42104a35c7b6e346a5a3077b2da463
Kartikey Kushwah <kartikey@koders.in>
2022-09-16 13:01:01 +0530

    Moved towards node-redmine

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
+const Redmine = require('node-redmine');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+DEBUG = True // ! Change to false in production
+
+const redmineObj = (host, api) => {
+    /**
+     * @param {string} host
+     * @param {string} api
+     * @returns {string}  the object of Redmine class is returned
+     */
+    const config = {
+      apiKey: api,
+    };
+    return new Redmine(host, config);
+};
+
+app.use(express.json())
+
+app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
+app.post('/milestones/', (req, res) => {
+    try{
+        const { apiKey, projectIdentifier } = req.body;
+ 
+        // protocol required in Hostname, supports both HTTP and HTTPS
+        const redmine = redmineObj("https://kore.koders.in", apiKey);
+         
+        redmine.projects({}, fetchData = (data, err) => {
+
+
+        })
+
+        // redmine.issues({limit: 2}, function(err, data) {
+        //   if (err) throw err;
+         
+        //   for (let i in data.issues) {
+        //     dump_issue(data.issues[i]);
+        //   }
+         
+        //   console.log('total_count: ' + data.total_count);
+        // });
+    }
+    catch (error) {
+        if (DEBUG === True)
+            console.log(`Something went wrong. Please try again. Reason ${error}`)
+        else
+            console.log("Something went wrong. Please try again.")
+    }
+
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
