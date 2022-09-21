3c2dc3e4f0fbbc0df2685f878d3de9746d8c9155
Kartikey Kushwah <kartikey@koders.in>
2022-09-21 21:55:03 +0530

    Circle Icon Added

--- src/component/dropdown/DropDown.tsx
+++ src/component/dropdown/DropDown.tsx
@@ -2,6 +2,8 @@ import { LinearProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CheckCircle from '@mui/icons-material/CheckCircle';
+import RemoveCircle from '@mui/icons-material/RemoveCircle';
 import Spacer from "../spacer/Spacer";
 
 const theme = createTheme({
@@ -29,36 +31,38 @@ interface Props {
 const DropDown = ({
   title,
   status,
-  mileStoneId,
   description,
   doneRatio,
   dueDate,
-  estimatedHours,
   issues,
-  spentHours,
 }: Props) => {
   const dueIn = () => {
-    let due = new Date(dueDate);
-    let today = new Date();
+    const due = new Date(dueDate);
+    const today = new Date();
     const diff = Math.ceil((due.getTime() - today.getTime()) / 86400000);
     return diff > 0 ? diff : 0;
   };
   return (
-    <Box width="90%">
-      <Typography>{title}</Typography>
-      <Typography fontSize="12px" >
-        {description ? description : ""}
-      </Typography>
-      <Spacer height={5} />
-      <Typography fontSize="12px">
-        {status === "closed" ? "Delievered" : dueDate && <> Delivering in {dueDate ? dueIn() === 1 ? dueIn() + " day" : dueIn() + " days" : "" }</>}
-      </Typography>
-      <ProgressBar
-        value={status !== "open" ? 100 : doneRatio / issues.length}
-        type="Progress"
-        status={status}
-      />
-    </Box>
+      <Box width="90%">
+      {status === "open" ? 
+        <Typography> Some Title </Typography> :
+        <div style={{ display: "flex"}}> <Typography marginRight="5px"> Title </Typography> <CheckCircle sx={{ fill: "#20B2AA !important"}} /> </div>
+      }
+      
+
+        <Typography fontSize="12px" >
+          {description ? description : ""}
+        </Typography>
+        <Spacer height={5} />
+        <Typography fontSize="12px">
+          {status === "closed" ? "Delievered" : dueDate && <> Delivering in {dueDate ? dueIn() === 1 ? dueIn() + " day" : dueIn() + " days" : "" }</>}
+        </Typography>
+        <ProgressBar
+          value={status !== "open" ? 100 : doneRatio / issues.length}
+          type="Progress"
+          status={status}
+        />
+      </Box>
   );
 };
 
@@ -78,43 +82,6 @@ const ProgressBar = ({ value, type, status }: { value: number; type: string, sta
           <Box sx={{ minWidth: 35 }} width="11%">
             <Typography variant="body2" color="white" fontSize="12px">
               {type}: {`${Math.round(value)}% `}
-            </Typography>
-          </Box>
-        </Box>
-      </ThemeProvider>
-    </>
-  );
-};
-
-const ProgressBars = ({
-  spentTime,
-  estimatedHours,
-  status,
-}: {
-  spentTime: number;
-  estimatedHours: number;
-  status: string;
-}) => {
-  console.log("first1", estimatedHours);
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box sx={{ width: "70%", mr: 1 }}>
-            <LinearProgress
-              variant="buffer"
-              value={
-                status !== "open" ? 100 : (spentTime / estimatedHours) * 100
-              }
-              valueBuffer={estimatedHours}
-              color={spentTime > estimatedHours ? "error" : "primary"}
-            />
-          </Box>
-          <Box sx={{ minWidth: 35 }} width="30%">
-            <Typography variant="body2" color="white" fontSize="12px">
-              {`Estimated: ${Math.round(estimatedHours)}h Spent: ${Math.round(
-                spentTime
-              )}h `}
             </Typography>
           </Box>
         </Box>
