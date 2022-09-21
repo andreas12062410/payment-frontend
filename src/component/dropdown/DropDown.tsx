b27f702207a6094cd55fa99bf821ceaaa52e2639
Kartikey Kushwah <kartikey@koders.in>
2022-09-21 20:08:14 +0530

    Added themeprovider

--- src/component/dropdown/DropDown.tsx
+++ src/component/dropdown/DropDown.tsx
@@ -1,6 +1,15 @@
 import { LinearProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#20B2AA',
+    }
+  },
+});
 
 interface Props {
   title: string;
@@ -53,20 +62,22 @@ export default DropDown;
 const ProgressBar = ({ value, type }: { value: number; type: string }) => {
   return (
     <>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Box sx={{ width: "70%", mr: 1 }}>
-          <LinearProgress
-            variant="buffer"
-            value={value}
-            valueBuffer={100}
-          />
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box sx={{ width: "70%", mr: 1 }}>
+            <LinearProgress
+              variant="buffer"
+              value={value}
+              valueBuffer={100}
+            />
+          </Box>
+          <Box sx={{ minWidth: 35 }} width="30%">
+            <Typography variant="body2" color="white" fontSize="12px">
+              {type}: {`${Math.round(value)}% `} 
+            </Typography>
+          </Box>
         </Box>
-        <Box sx={{ minWidth: 35 }} width="30%">
-          <Typography variant="body2" color="white" fontSize="12px">
-            {type}: {`${Math.round(value)}% `} 
-          </Typography>
-        </Box>
-      </Box>
+      </ThemeProvider>
     </>
   );
 };
@@ -80,21 +91,23 @@ const ProgressBars = ({
 }) => {
   return (
     <>
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Box sx={{ width: "70%", mr: 1 }}>
-          <LinearProgress
-            variant="buffer"
-            value={(spentTime / estimatedHours) * 100}
-            valueBuffer={estimatedHours}
-            color={spentTime > estimatedHours ? "error" : "primary"}
-          />
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ width: "70%", mr: 1 }}>
+              <LinearProgress
+                variant="buffer"
+                value={(spentTime / estimatedHours) * 100}
+                valueBuffer={estimatedHours}
+                color={spentTime > estimatedHours ? "error" : "primary"}
+              />
+            </Box>
+          <Box sx={{ minWidth: 35 }} width="30%">
+            <Typography variant="body2" color="white" fontSize="12px">
+              {`Estimated: ${Math.round(estimatedHours)}h Spent: ${Math.round(spentTime)}h `}
+            </Typography>
+          </Box>
         </Box>
-        <Box sx={{ minWidth: 35 }} width="30%">
-          <Typography variant="body2" color="white" fontSize="12px">
-            {`Estimated: ${Math.round(estimatedHours)}h Spent: ${Math.round(spentTime)}h `}
-          </Typography>
-        </Box>
-      </Box>
+      </ThemeProvider>
     </>
   );
 };
