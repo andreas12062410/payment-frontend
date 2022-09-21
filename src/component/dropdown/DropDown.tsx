360f1afb7a7c7a7b637df7e80ca3091466ae95a2
Kartikey Kushwah <kartikey@koders.in>
2022-09-21 20:58:36 +0530

    Fixed content and design structure in releases

--- src/component/dropdown/DropDown.tsx
+++ src/component/dropdown/DropDown.tsx
@@ -2,6 +2,7 @@ import { LinearProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Spacer from "../spacer/Spacer";
 
 const theme = createTheme({
   palette: {
@@ -39,44 +40,42 @@ const DropDown = ({
   const dueIn = () => {
     let due = new Date(dueDate);
     let today = new Date();
-    const diff = Math.round((due.getTime() - today.getTime()) / 86400000);
+    const diff = Math.ceil((due.getTime() - today.getTime()) / 86400000);
     return diff > 0 ? diff : 0;
   };
   return (
     <Box width="90%">
       <Typography>{title}</Typography>
-      <Typography>
-        Description: {description ? description : "N/A"}
-        <br />
-        {dueDate && <> Delivering in {dueDate ? dueIn() + " days" : "N/A"}</>}
+      <Typography fontSize="12px" >
+        {description ? description : ""}
+      </Typography>
+      <Spacer height={5} />
+      <Typography fontSize="12px">
+        {status === "closed" ? "Delievered" : dueDate && <> Delivering in {dueDate ? dueIn() === 1 ? dueIn() + " day" : dueIn() + " days" : "" }</>}
       </Typography>
       <ProgressBar
         value={status !== "open" ? 100 : doneRatio / issues.length}
         type="Progress"
+        status={status}
       />
-      {/* <ProgressBars
-        {...{
-          estimatedHours: estimatedHours,
-          spentTime: spentHours,
-          status: status,
-        }}
-      /> */}
     </Box>
   );
 };
 
 export default DropDown;
 
-const ProgressBar = ({ value, type }: { value: number; type: string }) => {
+const ProgressBar = ({ value, type, status }: { value: number; type: string, status: string }) => {
   console.log("first", value);
   return (
     <>
       <ThemeProvider theme={theme}>
         <Box sx={{ display: "flex", alignItems: "center" }}>
-          <Box sx={{ width: "70%", mr: 1 }}>
-            <LinearProgress variant="buffer" value={value} valueBuffer={100} />
+          <Box sx={{ width: "100%", mr: 1 }}>
+            { status === "open" ? 
+              <LinearProgress variant="buffer" value={value} valueBuffer={value} /> : <LinearProgress variant="determinate" value={value} /> 
+            }
           </Box>
-          <Box sx={{ minWidth: 35 }} width="30%">
+          <Box sx={{ minWidth: 35 }} width="11%">
             <Typography variant="body2" color="white" fontSize="12px">
               {type}: {`${Math.round(value)}% `}
             </Typography>
