49b5b922744fe2710b97ec8496603022c9aeecf2
andreas0624 <kindlyman0128@gmail.com>
2022-09-30 13:14:03 +0530

    change base url

--- src/helper/api.ts
+++ src/helper/api.ts
@@ -3,9 +3,7 @@ import Axios, { AxiosResponse } from "axios";
 type Endpoint = "/get-project" | "/get-budget" | "/checkout";
 
 const client = Axios.create({
-  // TODO=>
-  // baseURL: "https://api.koders.in",
-  baseURL: "http://192.168.29.117:9442/",
+  baseURL: "https://api.koders.in",
   headers: {
     "Content-Type": "application/json",
   },
