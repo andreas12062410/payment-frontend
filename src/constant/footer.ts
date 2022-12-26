29a690650d716dff4cae28f86229c8e80e9e6324
Shaloni Modi <panther@Shalonis-MacBook-Air.local>
2022-12-27 03:55:11 +0530

    update(links): Footer links

--- src/constant/footer.ts
+++ src/constant/footer.ts
@@ -1,14 +1,14 @@
 export const kFooterLink = [
   {
-    link: "https://koders.in/#/cancellation",
+    link: "https://koders.in/cancellation",
     title: "Cancellation Policy",
   },
   {
-    link: "https://koders.in/#/cancellation",
+    link: "https://koders.in/cancellation",
     title: "Refund Policy",
   },
   {
-    link: "https://koders.in/#/support",
-    title: "Customer Support",
+    link: "https://koders.in/privacy-policy",
+    title: "Privacy Policy",
   },
 ];
